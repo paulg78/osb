bdb46cab410200094cdd107a991032f3f0d4113e
Paulg <paulg78@hotmail.com>
2017-04-30 23:21:37 +0000

    cleaned up add student to schedule

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var passport = require("passport");
 var User = require("../models/user");
 var async = require('async');
 var crypto = require('crypto');
-var nodemailer = require('nodemailer');
+// var nodemailer = require('nodemailer');
 
 //root route
 router.get("/", function(req, res){
