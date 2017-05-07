4ea79ec70dbd957e0ef109171905d8e195ff370f
Paulg <paulg78@hotmail.com>
2017-05-07 06:45:32 +0000

    fixes and format good now

--- public/scripts/selectStudent.js
+++ public/scripts/selectStudent.js
@@ -5,6 +5,8 @@ $("[name='unschedStud']").on('click', function() {
     // too confusing and not useful to allow both a sheduled and unscheduled student to be active at same time
     $('.active').removeClass('active');    
     // $('.active').filter("[name='unschedStud']").removeClass('active');
+    // hide all the unschedule buttons
+    $("[name='remStudentBtn']").hide();
     $(this).toggleClass('active');
     $("[name='addStudentBtn']").attr("disabled",false);
 });
@@ -13,7 +15,7 @@ $("[name='unschedStud']").on('click', function() {
 $("[name='addStudentBtn']").on('click', function() {
     console.log("clicked add student");
     var dayId = $("#dayId").text(); 
-    var slotId= $(this).parent().parent().parent().find("[name='slotId']").text()
+    var slotId= $(this).parent().parent().children("[name='slotId']").text();
     var studentId= $('.active').filter("[name='unschedStud']").children("[name=studentId]").text();
     console.log("dayId=" + dayId);
     console.log("slotId=" + slotId);
@@ -24,11 +26,16 @@ $("[name='addStudentBtn']").on('click', function() {
 
 // Activate scheduled student and show remove button
 $("[name='schedStud']").on('click', function() {
+    // hide all the unschedule buttons
+    $("[name='remStudentBtn']").hide();
     // too confusing and not useful to allow both a sheduled and unscheduled student to be active at same time
     $('.active').removeClass('active');   
     // $('.active').filter("[name='schedStud']").removeClass('active');
     $(this).toggleClass('active');
-    $(this).parent().parent().find("[name='remStudentBtn']").removeClass('hidden');
+    $(this).parent().parent().find("[name='remStudentBtn']").show();
+    // disable add student buttons
+    $("[name='addStudentBtn']").attr("disabled",true);   
+    // $(this).parent().parent().find("[name='remStudentBtn']").removeClass('hidden');    
     //fix problem of item being clicked more than once (check if button already exists)
     // $(this).parent().children().append('<button type="button" id="removeStudentbtn">remove</button>');
     // // enable add student buttons
@@ -39,7 +46,7 @@ $("[name='schedStud']").on('click', function() {
 $("[name='remStudentBtn']").on('click', function() {
     console.log("clicked remove student");
     var dayId = $("#dayId").text(); 
-    var slotId= $(this).parent().parent().parent().find("[name='slotId']").text()
+    var slotId= $(this).parent().parent().children("[name='slotId']").text();
     var studentId= $('.active').filter("[name='schedStud']").children("[name=studentId]").text();
     console.log("dayId=" + dayId);
     console.log("slotId=" + slotId);
