079c878570ba8ed257b53f966df47605502f616f
Paulg <paulg78@hotmail.com>
2017-05-09 18:12:53 +0000

    removed cookieParser

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express"),
   bodyParser = require("body-parser"),
   mongoose = require("mongoose"),
   passport = require("passport"),
-  cookieParser = require("cookie-parser"),
+  // cookieParser = require("cookie-parser"),
   // LocalStrategy = require("passport-local"),
   flash = require("connect-flash"),
   Campground = require("./models/campground"),
@@ -37,7 +37,7 @@ app.use(bodyParser.urlencoded({
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride('_method'));
-app.use(cookieParser('secret'));
+// app.use(cookieParser('secret'));
 app.use(flash());
 
 // seedDB(); //seed the database
