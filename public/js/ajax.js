83d0151aea2bbc583afa5f1b097ef09518aa7a8c
Paulg <paulg78@hotmail.com>
2018-03-29 05:26:41 +0000

    reverted student list by school for scheduling in edit form

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,30 +1,6 @@
 'use strict';
 
 /* global $ */
-/* global Option */
-
-function dateString(d) {
-    var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    return dayNames[d.getDay()] + " " + (d.getMonth() + 1) + "/" + d.getDate() + "/" + (d.getFullYear() - 2000);
-}
-
-function timeString(d) {
-    var h = d.getUTCHours();
-    var ap;
-    if (h > 12) {
-        h = h - 12;
-        ap = "PM";
-    }
-    else {
-        ap = "AM";
-    }
-    var m = d.getMinutes();
-    if (m == 0) {
-        m = "00";
-    }
-    return h + ":" + m + " " + ap;
-}
-
 $('#newStudentForm').submit(function (e) {
     e.preventDefault();
     var studentStr = $(this).serialize();
@@ -42,8 +18,7 @@ $('#newStudentForm').submit(function (e) {
                 $('#addMsg').text(result.student.fname + " " + result.student.lname + " added.");
                 $('#newStudentForm').find('.form-control').val("");
                 $('#newStudentForm').find('.form-control').first().focus();
-            }
-            else {
+            } else {
                 if (result.msg != null) {
                     $('#addMsg').removeClass('failMsg successMsg');
                     $('#addMsg').addClass('failMsg');
@@ -69,66 +44,3 @@ $(".delStudBtn").on('click', function (e) {
         e.preventDefault();
     }
 });
-
-function getAvailSlots() {
-    // e.preventDefault();
-    $.ajax({
-        url: "/slots/avail",
-        type: 'GET',
-        success: function (slots) {
-            // console.log("slots returned=" + slots + ".");
-            // console.log("ajax callback executed; slots[0].sdate=" + slots[0].sdate);
-            $("#slotsAvail").text(slots);
-        }
-    });
-    // console.log("finished getAvailSlots");
-}
-
-$(".dateSched").on('show.bs.select', function (e) {
-    var slots = JSON.parse($("#slotsAvail").text());
-    var i, d, md;
-    var prevmd = "";
-    var sel = new Date(this.value);
-    var selmd = sel.getMonth() + "-" + sel.getDate();
-    // console.log("selmd=" + selmd);
-    for (i = 0; i < slots.length; i++) {
-        d = new Date(slots[i].sdate);
-        md = d.getMonth() + "-" + d.getDate();
-        if (md != prevmd && md != selmd) {
-            this.add(new Option(dateString(d), slots[i].sdate));
-            prevmd = md;
-            // console.log("dateString(d)=" + dateString(d));
-        }
-    }
-    $(this).selectpicker('refresh');
-});
-
-$(".dateSched").on('hidden.bs.select', function (e) {
-    $(this).children('option:not(:first)').remove();
-});
-
-$(".timeSched").on('show.bs.select', function (e) {
-    var slots = JSON.parse($("#slotsAvail").text());
-    console.log("selected date=" + $(this).parent().parent().prev().find(".dateSched")[1].value);
-
-    var i, d, md;
-    var sel = new Date($(this).parent().parent().prev().find(".dateSched")[1].value);
-    var selmd = sel.getMonth() + "-" + sel.getDate();
-    for (i = 0; i < slots.length; i++) {
-        d = new Date(slots[i].sdate);
-        md = d.getMonth() + "-" + d.getDate();
-        if (md == selmd) {
-            this.add(new Option(timeString(new Date(slots[i].sdate)), slots[i].sdate));
-        }
-    }
-    $(this).selectpicker('refresh');
-});
-
-$(".timeSched").on('hidden.bs.select', function (e) {
-    $(this).children('option:not(:first)').remove();
-});
-
-// $(".schedStdnt").on('changed.bs.select', function (e) {
-//     e.preventDefault();
-//     console.log("changed schedule from " + this.oldValue + " to " + this.value);
-// });
